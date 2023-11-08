e73c2a9e2f7e7196690b3ec50bd057fe02ca077a
Diangelion <bryanrichie92@gmail.com>
2023-11-08 13:40:05 +0700

    login & register get value done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,45 +14,120 @@ import React, { useContext, useRef, useState } from "react";
 
 import "../styles/Login.css";
 
+interface LoginDataProps {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
+  const [loginData, setLoginData] = useState<LoginDataProps>({
+    email: "",
+    password: "",
+  });
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState<boolean>();
+
+  const validateEmail = (email: string) => {
+    return email.match(
+      /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+  };
+
+  const validate = (ev: Event) => {
+    const value = (ev.target as HTMLInputElement).value;
+    setIsValid(undefined);
+    if (value === "") return;
+    validateEmail(value) !== null ? setIsValid(true) : setIsValid(false);
+  };
+
+  const markTouched = () => {
+    setIsTouched(true);
+  };
+
+  const handleInputChange = (ev: Event) => {
+    const { value, name } = ev.target as HTMLInputElement;
+    setLoginData((prevLoginData) => ({
+      ...prevLoginData,
+      [name]: value,
+    }));
+  };
+
+  console.log(loginData);
+
   return (
     <IonPage>
-      <IonHeader></IonHeader>
-      <IonContent color="dark" className="ion-text-center">
-        <div className="border-logo">
+      <IonHeader>
+        <div className="border-logo ion-text-center">
           <IonLabel className="logo">TeKaRis</IonLabel>
           <IonLabel className="judul">Login</IonLabel>
         </div>
+      </IonHeader>
+
+      {/* <IonContent color="dark" className="ion-text-center"> */}
+      <IonContent className="ion-padding ">
         <IonGrid>
-          <IonRow>
+          <IonRow className="ion-margin-bottom">
             <IonCol>
-              <IonItem className="input" color="medium">
-                <IonLabel position="floating">Enter your email</IonLabel>
-                <IonInput></IonInput>
-              </IonItem>
+              {/* <IonLabel position="floating">Enter your email</IonLabel>
+              <IonInput></IonInput> */}
+              <IonInput
+                className={`${isValid && "ion-valid"} ${
+                  isValid === false && "ion-invalid"
+                } ${isTouched && "ion-touched"}`}
+                type="email"
+                fill="solid"
+                label="Email"
+                labelPlacement="floating"
+                helperText="Enter a valid email"
+                errorText="Invalid email"
+                onIonInput={(event) => {
+                  validate(event);
+                  handleInputChange(event);
+                }}
+                onIonBlur={() => markTouched()}
+              ></IonInput>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
-              <IonItem className="input" color="medium">
-                <IonLabel position="floating">Enter your password </IonLabel>
-                <IonInput></IonInput>
-              </IonItem>
+              {/* <IonLabel position="floating">Enter your password </IonLabel>
+                <IonInput></IonInput> */}
+              <IonInput
+                type="password"
+                fill="solid"
+                label="Password"
+                labelPlacement="floating"
+                helperText="Enter your password"
+                onIonInput={(event) => {
+                  handleInputChange(event);
+                }}
+                onIonBlur={() => markTouched()}
+              ></IonInput>
             </IonCol>
           </IonRow>
-          <IonRow>
+
+          <IonRow className="ion-margin-vertical">
             <IonCol>
-              <IonButton routerLink="/home">Login</IonButton>
+              <IonButton routerLink="/home" size="default">
+                Login
+              </IonButton>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
               <IonLabel className="text-font">
                 Does not have an account?{" "}
               </IonLabel>
-              <IonButton routerLink="/register" className="text-font">
+              <a href="/register">Register</a>
+              {/* <IonButton
+                routerLink="/register"
+                className="text-font"
+                fill="clear"
+              >
                 Register
-              </IonButton>
+              </IonButton> */}
             </IonCol>
           </IonRow>
         </IonGrid>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,74 +5,210 @@ import {
   IonGrid,
   IonHeader,
   IonInput,
-  IonItem,
   IonLabel,
   IonPage,
   IonRow,
-  IonTitle,
-  IonToolbar,
 } from "@ionic/react";
-import React, { useContext, useRef, useState } from "react";
+import React, { useState } from "react";
 
 import "../styles/Register.css";
 
+interface RegisterDataProps {
+  name: string;
+  email: string;
+  password: string;
+  confirmationPassword: string;
+}
+
 const Register: React.FC = () => {
+  const [registerData, setRegisterData] = useState<RegisterDataProps>({
+    name: "",
+    email: "",
+    password: "",
+    confirmationPassword: "",
+  });
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState<boolean>();
+  const [isPasswordMatch, setIsPasswordMatch] = useState<boolean>();
+
+  const emailValidation = (email: string) => {
+    return email.match(
+      /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+  };
+
+  const validateEmail = (ev: Event) => {
+    const value = (ev.target as HTMLInputElement).value;
+    setIsValidEmail(undefined);
+    if (value === "") return;
+    emailValidation(value) !== null
+      ? setIsValidEmail(true)
+      : setIsValidEmail(false);
+  };
+
+  const validatePassword = (ev: Event) => {
+    const { value, name } = ev.target as HTMLInputElement;
+    if (
+      value === "" ||
+      registerData.confirmationPassword === "" ||
+      registerData.password === ""
+    ) {
+      setIsPasswordMatch(true);
+      return;
+    }
+
+    if (name === "password") {
+      setIsPasswordMatch(value === registerData.confirmationPassword);
+    } else if (name === "confirmationPassword") {
+      setIsPasswordMatch(value === registerData.password);
+    }
+  };
+
+  const markTouched = () => {
+    setIsTouched(true);
+  };
+
+  const handleInputChange = (ev: Event) => {
+    const { value, name } = ev.target as HTMLInputElement;
+    setRegisterData((prevRegisterData) => ({
+      ...prevRegisterData,
+      [name]: value,
+    }));
+  };
+
   return (
     <IonPage>
-      <IonHeader></IonHeader>
-      <IonContent color="dark" className="ion-text-center">
-        <div className="border-logo">
+      <IonHeader>
+        <div className="border-logo ion-text-center">
           <IonLabel className="logo">TeKaRis</IonLabel>
           <IonLabel className="judul">Register</IonLabel>
         </div>
+      </IonHeader>
+
+      {/* <IonContent color="dark" className="ion-text-center"> */}
+      <IonContent className="ion-padding">
         <IonGrid>
           <IonRow>
             <IonCol>
-              <IonItem className="input" color="medium">
+              {/* <IonItem className="input" color="medium">
                 <IonLabel position="floating">Enter your name</IonLabel>
                 <IonInput></IonInput>
-              </IonItem>
+              </IonItem> */}
+              <IonInput
+                type="text"
+                fill="solid"
+                label="Name"
+                labelPlacement="floating"
+                helperText="Enter your name"
+                name="name"
+                // value={registerData.name}
+                onIonBlur={() => markTouched()}
+                onIonInput={(e) => handleInputChange(e)}
+              ></IonInput>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
-              <IonItem className="input" color="medium">
+              {/* <IonItem className="input" color="medium">
                 <IonLabel position="floating">Enter your email </IonLabel>
                 <IonInput></IonInput>
-              </IonItem>
+              </IonItem> */}
+              <IonInput
+                className={`${isValidEmail && "ion-valid"} ${
+                  isValidEmail === false && "ion-invalid"
+                } ${isTouched && "ion-touched"}`}
+                type="email"
+                fill="solid"
+                label="Email"
+                labelPlacement="floating"
+                helperText="Enter a valid email"
+                errorText="Invalid email"
+                name="email"
+                // value={registerData.email}
+                onIonInput={(e) => {
+                  validateEmail(e);
+                  handleInputChange(e);
+                }}
+                onIonBlur={() => markTouched()}
+                // onIonChange={(e) => handleInputChange(e)}
+              ></IonInput>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
-              <IonItem className="input" color="medium">
+              {/* <IonItem className="input" color="medium">
                 <IonLabel position="floating">Enter your password</IonLabel>
                 <IonInput></IonInput>
-              </IonItem>
+              </IonItem> */}
+              <IonInput
+                className={`${isPasswordMatch && "ion-valid"} ${
+                  isPasswordMatch === false && "ion-invalid"
+                } ${isTouched && "ion-touched"}`}
+                type="password"
+                fill="solid"
+                label="Password"
+                labelPlacement="floating"
+                helperText="Enter your password"
+                errorText="Password and Confirmation Password does not match"
+                name="password"
+                // value={registerData.password}
+                onIonInput={(e) => {
+                  handleInputChange(e);
+                  validatePassword(e);
+                }}
+                onIonBlur={() => markTouched()}
+                // onIonChange={(e) => handleInputChange(e)}
+              ></IonInput>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
-              <IonItem className="input" color="medium">
+              {/* <IonItem className="input" color="medium">
                 <IonLabel position="floating">
                   Enter your coofirm password
                 </IonLabel>
                 <IonInput></IonInput>
-              </IonItem>
+              </IonItem> */}
+              <IonInput
+                className={`${isPasswordMatch && "ion-valid"} ${
+                  isPasswordMatch === false && "ion-invalid"
+                } ${isTouched && "ion-touched"}`}
+                type="password"
+                fill="solid"
+                label="Confirmation Password"
+                labelPlacement="floating"
+                helperText="Confirm your password"
+                errorText="Password and Confirmation Password does not match"
+                name="confirmationPassword"
+                // value={registerData.confirmationPassword}
+                onIonInput={(e) => {
+                  handleInputChange(e);
+                  validatePassword(e);
+                }}
+                onIonBlur={() => markTouched()}
+                // onIonChange={(e) => handleInputChange(e)}
+              ></IonInput>
             </IonCol>
           </IonRow>
-          <IonRow>
+
+          <IonRow className="ion-margin-vertical">
             <IonCol>
               <IonButton routerLink="/home">Register</IonButton>
             </IonCol>
           </IonRow>
+
           <IonRow>
             <IonCol>
               <IonLabel className="text-font">
                 Already have an account?{" "}
               </IonLabel>
-              <IonButton routerLink="/login" className="text-font">
+              {/* <IonButton routerLink="/login" className="text-font">
                 Login
-              </IonButton>
+              </IonButton> */}
+              <a href="/login">Login</a>
             </IonCol>
           </IonRow>
         </IonGrid>
